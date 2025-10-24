0f11b00311f438e01f4c9df92b411ad2663e84c2
Arm2201 <armawesome22@gmail.com>
2025-10-24 14:15:13 +0700

    update for 24/10/25

--- day-2.py
+++ day-2.py
@@ -1,2 +1,28 @@
 print('Hello World!')
 print('Goodbye')
+# this is a comment (ctrl+/)
+# function_name(input_1, input_2, input_3) -> arguement
+print(1,2,6, 'Hello')
+print(1,2,6, 'Hello', sep='-')  
+print(1,2,6, 'Hello', sep='-t-')  
+
+# <----Data Types---->
+# number of students, names,the money, yes or no
+# number
+# interger
+3, 5, -4, 201
+
+# float
+3.4, 5.6, -4.2, 201.3
+
+# text -> string (enclose with quotation)
+'Hello World', 'Goodbye', 'I am changing this file as well.'
+
+# Boolean (truth value)
+True, False
+
+# NoneType
+None
+
+
+print(type(56))
